4c6c89f1abe5a1d248dc349bb5167efda7b72345
Subrata Sen <subrata.sen@ibm.com>
2024-01-08 15:52:39 +0530

    Firsr commit for whole code base for Sutainability dashboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import "./App.css";
+import "font-awesome/css/font-awesome.min.css";
+import { ProductService } from "./Services/mineDataServices";
+import logo from "../src/img/logo.png";
+import { FilterMatchMode, FilterOperator } from "primereact/api";
+import Criteria from "./components/criteria/criteria";
+import Matching from "./components/matching/matching";
 
-function App() {
+function App() { 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="grid-container">
+        <header className="header">
+          <div className="header_main">
+            <img
+              src={logo}
+              className="logo"
+              height="20"
+              width="20"
+              alt="logo"
+            />
+          </div>
+          <div className="header__avatar"></div>
+          <div className="navBar">
+            <ul className="sidenav__list">
+              <li className="sidenav__list-item">
+                <i className="fa fa-tasks" /> Dashboard
+              </li>
+              <li className="sidenav__list-item tooltip">
+                <i className="fa fa-exchange" /> Allocation
+                <ul className="tooltiptext">
+                  <li>
+                    <NavLink
+                      to="/"
+                      className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "active" : ""
+                      }
+                    >
+                      Criteria
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/matching"
+                      className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "active" : ""
+                      }
+                    >
+                      Matching
+                    </NavLink>
+                  </li>
+                </ul>
+              </li>
+              <li className="sidenav__list-item">
+                <i className="fa fa-bookmark-o" /> Credit
+              </li>
+            </ul>
+          </div>
+          <div className="header__avatar"></div>
+        </header>
+
+        <Routes>
+          <Route exact path="/" element={<Criteria />}></Route>
+          <Route exact path="/matching" element={<Matching />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { PrimeReactProvider } from 'primereact/api';
 import './index.css';
 import App from './App';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';  
+import 'primereact/resources/primereact.css';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render( 
+    <BrowserRouter>
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
+    </BrowserRouter> 
 );
 
 // If you want to start measuring performance in your app, pass a function
